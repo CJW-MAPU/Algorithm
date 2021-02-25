64e4e962f4f4355cd5e95f9d0745f6e37d735ae2
CJW-MAPU <c.j.biz.woong@gmail.com>
2021-02-25 22:13:31 +0900

    #solved This Is Coding Test

---
+++ src/TICT/page92_Refactor.java
@@ -0,0 +1,47 @@
+package TICT;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class page92_Refactor {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String inputValue = br.readLine();
+        String inputList = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(inputValue);
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        List<Integer> list = new ArrayList<>();
+
+        st = new StringTokenizer(inputList);
+
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        list.sort(Comparator.reverseOrder());
+
+        int first = list.get(0);
+        int second = list.get(1);
+
+        int MaxValueCount = (M / (K + 1)) * K;
+        MaxValueCount += M % (K + 1);
+
+        int result = MaxValueCount * first;
+        result += (M - MaxValueCount) * second;
+
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+    }
+}
